c040d4cc921ec1196d974e1c3e97eb55b933c251
Ubuntu <ubuntu@ip-172-31-95-156.ec2.internal>
2020-02-24 16:51:54 +0000

    modified aws_ec2.tf file

--- aws_ec2/aws_ec2.tf
+++ aws_ec2/aws_ec2.tf
@@ -1,8 +1,6 @@
 provider "aws" {
   region = "us-east-1"
   version = ">=0.12.8"
-  profile = var.profile
-  shared_credential_file = var.creds
 }
 
 locals {
@@ -14,11 +12,11 @@ locals {
 }
 
 module "ec2_instance" {
-  source = "../modules/ec2"
+  source = "github.com/vasu04/aws_terraform_modules/modules/ec2"
   subnet = ""
   security_group = ""
   public_ip = "false"
-  ec2_count = "2"
+  ec2_count = "1"
   instance_type = "t2.micro"
   tags = local.common_tags
   key_name = ""
@@ -26,17 +24,25 @@ module "ec2_instance" {
 }
 
 module "classic_lb" {
-  source = "../modules/elb"
-  lb_name = "test-lb"
+  source = "github.com/vasu04/aws_terraform_modules/modules/elb"
+  lb_name = ""
   target_health = "HTTP:80/test/version"
   lb_security_group = ""
   subnet_id = ""
-  #instances = module.ec2_instance.instance_ids
   tags = local.common_tags
 }
 
 module "lb_attachment" {
-  source = "../modules/elb_attachment"
+  source = "github.com/vasu04/aws_terraform_modules/modules/elb_attachment"
   elb = module.classic_lb.name
   instance = module.ec2_instance.instance_ids
+}
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = ""
+    region         = "us-east-1"
+    encrypt        = true
+  }
 }
